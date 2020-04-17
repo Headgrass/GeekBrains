d07766693e76c540608739be3a0eb0febf87b5d5
Headgrass <headgrass@ngs.ru>
2020-04-17 08:05:21 +0700

    Решение проблемы сохранения данных при помощи манифеста(запрет пересоздания активити)

--- Weather/app/src/main/java/ru/GeekBrains/weather/MainActivity.java
+++ Weather/app/src/main/java/ru/GeekBrains/weather/MainActivity.java
@@ -1,21 +1,38 @@
 package ru.GeekBrains.weather;
 //Продолжаю работать
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnCity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCity = findViewById(R.id.btnCity);
+
+        Button btnCity = findViewById(R.id.btnCity);
+
+        btnCity.setOnClickListener(changeCity);
+
 
         }
+    private  View.OnClickListener changeCity = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, Activitytwo.class);
+            startActivity();
+        }
+
+        private void startActivity() {
+            setContentView(R.layout.settings);
+        }
+    };
     }
 
 
