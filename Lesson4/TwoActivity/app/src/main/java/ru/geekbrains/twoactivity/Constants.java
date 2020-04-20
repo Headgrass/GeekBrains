e70ccc05283c24e7ae289e5d47ea58b59a40c1a0
Headgrass <headgrass@ngs.ru>
2020-04-20 22:50:52 +0700

    Передача данных во вторую активити

---
+++ Lesson4/TwoActivity/app/src/main/java/ru/geekbrains/twoactivity/Constants.java
@@ -0,0 +1,5 @@
+package ru.geekbrains.twoactivity;
+
+public interface Constants {
+    String TEXT = "ru.geekbrains.twoactivity";
+}
--- Lesson4/TwoActivity/app/src/main/java/ru/geekbrains/twoactivity/SecondActivity.java
+++ Lesson4/TwoActivity/app/src/main/java/ru/geekbrains/twoactivity/SecondActivity.java
@@ -5,13 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements Constants{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        String text = getIntent().getExtras().getString(TEXT);
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText(text);
 
         Button mainAct = findViewById(R.id.button2);
         mainAct.setOnClickListener(new View.OnClickListener() {
