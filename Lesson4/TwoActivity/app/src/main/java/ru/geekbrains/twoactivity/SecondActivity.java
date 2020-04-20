85afb9802304ed7b80b210e754ce2ce60401bee9
Headgrass <headgrass@ngs.ru>
2020-04-20 22:24:02 +0700

    Возврат по кнопке из второй активити

--- Lesson4/TwoActivity/app/src/main/java/ru/geekbrains/twoactivity/SecondActivity.java
+++ Lesson4/TwoActivity/app/src/main/java/ru/geekbrains/twoactivity/SecondActivity.java
@@ -1,7 +1,10 @@
 package ru.geekbrains.twoactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button mainAct = findViewById(R.id.button2);
+        mainAct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
