dba98bf2adf2344fe5dd8e086028ac8f17b897a1
Headgrass <headgrass@ngs.ru>
2020-04-28 01:08:17 +0700

    Окно логина с паролем

---
+++ Lesson5/FragmentManager/app/src/main/java/ru/geekbrains/fragmentmanager/Fragment3.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.fragmentmanager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_3, container, false);
+    }
+}
---
+++ Lesson5/FragmentManager/app/src/main/java/ru/geekbrains/fragmentmanager/MainActivity.java
@@ -0,0 +1,114 @@
+package ru.geekbrains.fragmentmanager;
+
+import android.net.sip.SipSession;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Fragment1 fragment1;
+    private Fragment2 fragment2;
+    private Fragment3 fragment3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        Button add1 = findViewById(R.id.add1);
+        add1.setOnClickListener(new ListenerOnAdd(fragment1));
+        Button add2 = findViewById(R.id.add2);
+        add2.setOnClickListener(new ListenerOnAdd(fragment2));
+        Button add3 = findViewById(R.id.add3);
+        add3.setOnClickListener(new ListenerOnAdd(fragment3));
+        Button remove1 = findViewById(R.id.remove1);
+        remove1.setOnClickListener(new ListenerOnRemove(fragment1));
+        Button remove2 = findViewById(R.id.remove2);
+        remove2.setOnClickListener(new ListenerOnRemove(fragment2));
+        Button remove3 = findViewById(R.id.remove3);
+        remove3.setOnClickListener(new ListenerOnRemove(fragment3));
+        Button replace1 = findViewById(R.id.replace1);
+        replace1.setOnClickListener(new ListenerOnReplace(fragment1));
+        Button replace2 = findViewById(R.id.replace2);
+        replace2.setOnClickListener(new ListenerOnReplace(fragment2));
+        Button replace3 = findViewById(R.id.replace3);
+        replace3.setOnClickListener(new ListenerOnReplace(fragment3));
+        findViewById(R.id.return1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
+
+    class ListenerOnAdd implements View.OnClickListener {
+        Fragment fragment;
+
+        ListenerOnAdd(Fragment fragment) {
+            this.fragment = fragment;
+        }
+
+        @Override
+        public void onClick(View v) {
+            addFragment();
+        }
+
+        //добавить фрагмент
+        private void addFragment() {
+            //открыть транзакцию
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            //добавить фрагмент
+            fragmentTransaction.add(R.id.fragment_container, fragment);
+            //закрыть транзакцию
+            fragmentTransaction.commit();
+        }
+    }
+
+    class ListenerOnRemove implements View.OnClickListener {
+        Fragment fragment;
+
+        ListenerOnRemove(Fragment fragment) {
+            this.fragment = fragment;
+        }
+
+        @Override
+        public void onClick(View v) {
+            removeFragment();
+        }
+
+        //удалить фрагмент
+        private void removeFragment() {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.remove(fragment);
+            fragmentTransaction.commit();
+        }
+    }
+
+    class ListenerOnReplace implements View.OnClickListener {
+        Fragment fragment;
+
+        ListenerOnReplace(Fragment fragment) {
+            this.fragment = fragment;
+        }
+
+        @Override
+        public void onClick(View v) {
+            replaceFragment();
+        }
+
+        //заменить фрагмент
+        private void replaceFragment() {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+        }
+    }
+}
